6761200b0493becc07b39efc0cd934b1205b1a5d
TheEggHealer <TheEggKiller@gmail.com>
2021-05-05 16:27:23 +0200

    Added dependencies for react navigation

---
+++ styles/GlobalStyles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  headerText: {
+    fontFamily: 'red-hat-bold',
+    fontSize: 24,
+    color: 'black',
+  }
+});
